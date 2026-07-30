d5389ee3bc72ca6efbe0a4e9bdee3322c60653f3
OKJOIZJ <OKJOIZJ@users.noreply.github.com>
2026-07-30 16:38:55 +0900

    release: v3.12.51 planner hierarchy spacing + marker/time UI batch (v3.12.44c–51)
    - index.html sha1 2e6ecd0e6101ec4c1769506f7485d27070843265
    - sw.js sha1 b9f213cbef59a4ce413d5369f611996df99f4a14
    - SW cache timegrid-v3.12.51-20260730
    - Includes: 44c memo AC, todo time markers, marker crashfix, planner row unify, todo UX, marker/time chips, optical center, neutral todo chip border, untimed align, hierarchy spacing 18/4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "timegrid-v3.12.43b-20260729";
+const VERSION = "timegrid-v3.12.51-20260730";
 const SHELL_CACHE = `${VERSION}-shell`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const APP_SHELL = [
