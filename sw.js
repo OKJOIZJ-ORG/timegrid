79fd607b2876bc299c7f8c23b566db74d1e72fa4
OKJOIZJ <yundda2@gmail.com>
2026-08-21 00:52:27 +0900

    fix(ui): v3.12.66 - fix todo action menu trash icon bottom clipping & optical centering

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "timegrid-v3.12.65-20260820";
+const VERSION = "timegrid-v3.12.66-20260821";
 const SHELL_CACHE = `${VERSION}-shell`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const APP_SHELL = [
